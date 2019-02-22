34cec52218f89b9698168494b729a3050c6a0332
Nikola Stojkovic <dragstor@gmail.com>
2019-02-22 22:00:18 +0100

    Initial Commit

---
+++ Sound Re-Enabler/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Sound Re-Enabler
+//
+//  Created by Nikola on 2/22/19.
+//  Copyright © 2019 Stojković. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Sound Re-Enabler/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Sound Re-Enabler
+//
+//  Created by Nikola on 2/22/19.
+//  Copyright © 2019 Stojković. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
