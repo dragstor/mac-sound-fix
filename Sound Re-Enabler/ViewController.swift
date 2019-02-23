9afac90a9481676cf06d2b55cacb06c5e46f75d7
Nikola Stojkovic <dragstor@gmail.com>
2019-02-23 04:09:14 +0100

    Done

--- Sound Re-Enabler/ViewController.swift
+++ Sound Re-Enabler/ViewController.swift
@@ -8,12 +8,16 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
-
+class ViewController: NSViewController, NSWindowDelegate {
+    @IBOutlet weak var btnFix: NSButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        NSAlert.showAlert(title: "Information", message: "Authentication is required in order to fix the sound bug.\n\nIt is required because of utilizing the killall command, which requires the sudo.", style: .informational)
+        
     }
 
     override var representedObject: Any? {
@@ -21,7 +25,46 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+    
 
+    
+    @IBAction func btnFix(_ sender: Any?) {
+        if fix() {
+            NSAlert.showAlert(title: "🎵 Let the music play...", message: "We did it! 🥳\n\nExiting now 🥰", style: .informational)
+            NSApp.terminate(self)
+        } else {
+            NSAlert.showAlert(title: "We didn't make it. 😢", message: "😭 I have failed you.", style: .critical)
+            
+        }
+    }
+    
+    func fix() -> Bool {
+        let taskTwo = Process()
+        taskTwo.launchPath = "/usr/bin/sudo"
+        taskTwo.arguments = ["-S", "/usr/bin/killall", "coreaudiod"]
+        
+        taskTwo.launch()
+        taskTwo.waitUntilExit()
+        
+        if taskTwo.terminationStatus == 0 {
+            return true
+        } else {
+            return false
+        }
+    }
 
 }
 
+extension NSAlert {
+    static func showAlert(title: String?, message: String?, style: NSAlert.Style = .informational) {
+        let alert = NSAlert()
+        if let title = title {
+            alert.messageText = title
+        }
+        if let message = message {
+            alert.informativeText = message
+        }
+        alert.alertStyle = style
+        alert.runModal()
+    }
+}
